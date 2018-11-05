215374f19dbcdb6d359c009505c94bfc1eb28d58
Sebass van Boxel <svanboxel@github.com>
2018-11-05 15:01:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ var server = http.createServer(function(request, response) {
 var port = process.env.PORT || 1337
 server.listen(port)
 
-console.log('Server running at http://localhost:%d', port)
+// console.log('Server running at http://localhost:%d', port)
 
