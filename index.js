c47aef42aadf5c83e91a31dd093c6fa5e8504f04
Sebass van Boxel <svanboxel@github.com>
2018-11-05 13:51:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var http = require('http')
 
 var server = http.createServer(function(request, response) {
   response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World!')
+  response.write('Hello World!')
+  response.write(`Server time: ${new Date(Date.now()).toLocaleString()}`)
+  response.end();
 })
 
 var port = process.env.PORT || 1337
 server.listen(port)
 
 console.log('Server running at http://localhost:%d', port)
+
