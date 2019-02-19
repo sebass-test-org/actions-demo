a8af1712055de68058d99e38fcb7c4638532cd72
Sebass van Boxel <svboxel@gmail.com>
2019-02-19 20:33:15 +0100

    fixed some linting issues

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-var http = require('http')
+const http = require('http');
+const fs = require('fs');
 
-var server = http.createServer((request, response) => {
+const server = http.createServer(() => {
   fs.readFile('./index.html', (err, html) => {
     if (err) {
-      throw err; 
-    }       
-    http.createServer((request, response) => {  
-      response.writeHeader(200, {"Content-Type": "text/html"});  
-      response.write(html);  
+      throw err;
+    }
+    http.createServer((request, response) => {
+      response.writeHeader(200, { 'Content-Type': 'text/html' });
+      response.write(html);
       response.write(`Server time: ${new Date(Date.now()).toLocaleString()}`)
-      response.end();  
+      response.end();
     }).listen(8000);
   });
 });
-var port = process.env.PORT || 1337
-server.listen(port)
 
+const port = process.env.PORT || 1337
+server.listen(port)
 
 // console.log('Server running at http://localhost:%d', port)
 
